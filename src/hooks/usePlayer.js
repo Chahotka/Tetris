e8ff8ece660e78297eb610d68aab015e842ef6ad
Arslan Rasulov <magomrak@yandex.ru>
2023-07-25 16:39:14 +0200

    movement, collision, rotate, for tetrominos

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,52 @@
 import { useCallback, useState } from 'react'
 import { TETROMINOS, randomTetromino } from '../tetrominos'
-import { STAGE_WIDTH } from '../gameHelpers'
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers'
 
 function usePlayer() {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
     tetromino: TETROMINOS[0].shape,
+    collided: false
   })
+
+  const rotate = (matrix, dir) => {
+    // Делает ряды колоннами
+    console.log(matrix)
+    const rotatedTetro = matrix.map((_, index) => {
+      return matrix.map(col => col[index])
+    })
+
+    console.log(rotatedTetro)
+
+    // Переворачивает ряды в обратном порядке
+    if (dir > 0) {
+      return rotatedTetro.map(row => row.reverse())
+    }
+
+    rotatedTetro.reverse()
+  }
+
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player))
+
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir)
+
+    const pos = clonedPlayer.pos.x
+    let offset = 1
+    
+    while(checkCollision(clonedPlayer, stage, { x: 0, y: 0})) {
+      clonedPlayer.pos.x += offset
+      offset = -(offset + (offset > 0 ? 1 : -1))
+
+      if (offset > clonedPlayer.tetromino[0].lenght) {
+        rotate(clonedPlayer.tetromino, -dir)
+        clonedPlayer.pos.x = pos
+        return;
+      }
+    }
+
+    setPlayer(clonedPlayer)
+  }
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
@@ -30,9 +70,9 @@ function usePlayer() {
     })
   }, [])
 
-  
+  // resetPlayer ебет мозгу и по какой-то причине не работает
 
-  return { player, updatePlayerPos, resetPlayer }
+  return { player, updatePlayerPos, resetPlayer, playerRotate }
 }
 
 export default usePlayer
